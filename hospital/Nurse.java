b7b039729966e6e979b9212e6ff804ac9ab3c4e0
aGomez9907 <118466508+aGomez9907@users.noreply.github.com>
2022-11-29 09:50:30 -0300

    Delete hospital directory

--- hospital/Nurse.java
+++
@@ -1,9 +0,0 @@
-package hospital;
-
-public class Nurse extends Person {
-    public Nurse(String name, int age, String nationality){
-        setAge(age);
-        setName(name);
-        setNationality(nationality);
-    }
-}
