bd9ee84fa80c63dde0def6d2669b9770bbaaa87b
aGomez9907 <agomez_@live.com.ar>
2022-12-07 22:19:21 -0300

    added logger

--- solvd/src/solvd/firsttask/hello/world/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package solvd.firsttask.hello.world;
-
-public class HelloWorld {
-        public static void main(String[] args){
-            System.out.println("Hello World" + args[0]);
-            System.out.println("My name is: "+ args[1]);
-        }
-    }
-
